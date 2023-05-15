e852ca661079d3f0e3453f2d500b63c29f9c0a5d
twdeveloper <twdeveloper@outlook.com>
2023-05-15 13:47:07 +0800

    v1

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using isRock.Core.AOP;
+
+var bc = new BmiCalculator();
+
+bc.Height = 170;
+bc.Weight = 70;
+
+var BmiResult = bc.Calculate();
+Console.WriteLine($"BMI:{BmiResult}");
+
+public class BmiCalculator  
+{
+    public int Weight { get; set; }
+    public int Height { get; set; }
+    public decimal BMI
+    {
+        get
+        {
+            return Calculate();
+        }
+    }
+
+    public decimal Calculate()
+    {
+        decimal result = 0;
+        //將float改成int試試看
+        decimal height = (decimal)Height / 100;
+        result = Weight / (height * height);
+
+        return result;
+    }
+}
